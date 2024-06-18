b5c8d6df5a9d698345548f2ff4c180655dbae50e
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-06-18 17:44:09 -0300

    v2.0
    adicinonando novas funções no index.html, style.css e add o script.js e orders.js
    
    Co-Authored-By: Emersonlaofc <172435277+Emersonlaofc@users.noreply.github.com>
    Co-Authored-By: Hellenlbdev <162762219+Hellenlbdev@users.noreply.github.com>

---
+++ orders.js
@@ -0,0 +1,8 @@
+const Orders = [
+    {
+        ProductName: ''
+        productNumber:
+        paymenStatus:
+        shipping:
+    }
+]
---
+++ script.js
@@ -0,0 +1,22 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themetoggler = document.queryselector (".themetoggler")
+
+// show sidebar
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+// close sidebar
+closeBtn.addEventListener ('click', () => {
+    sideMenu.style.display = 'none'
+})
+
+// change theme
+themetoggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themetoggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themetoggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
