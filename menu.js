543990a52cc451d43c7a5840e377444979dbafab
Emersonlaofc <limaemerson001@gmail.com>
2024-06-20 17:00:21 -0300

    v7.0

---
+++ menu.js
@@ -0,0 +1,12 @@
+//qunado o botão "menu-btn" for clicado exibir o setar o display do aside como block
+    document.getElementById('menu-btn').addEventListener('click', () => {
+        const menu = document.getElementById('aside')
+        menu.style.display = menu.style.display === 'block'? 'none' : 'block';
+        menu.style.height = '100%'; 
+    });
+
+    document.getElementById('close-btn').addEventListener('click', () => {
+        const menu = document.getElementById('aside')
+        menu.style.display = 'none';
+        menu.style.height = '0'; 
+    });
